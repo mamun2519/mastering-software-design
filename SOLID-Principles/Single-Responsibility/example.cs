d207025e1b16f9e454cecb55d6ef0886b395275a
mamun2519 <juborajislam46@gmail.com>
2025-07-09 22:36:45 +0600

    added to single responsibility pattern

--- SOLID-Principles/Single-Responsibility/example.cs
+++ SOLID-Principles/Single-Responsibility/example.cs
@@ -4,29 +4,9 @@ using System;
 
 
 
-class GenerateFDF {
-    public void Generate(string invoiceNumber){
-        Console.WriteLine("Generating FDF for invoice {0}", invoiceNumber);
-    }
-}
 
-class InvoiceProcessor {
-        private SendEmail _sendEmail;
-        private GenerateFDF _generateFDF;
-    public void Process(Invoice invoice){
-        Console.WriteLine("Processing invoice {0}", invoice.InvoiceNumber);
-        Console.WriteLine("Invoice date {0}", invoice.InvoiceDate);
-        Console.WriteLine("Invoice amount {0}", invoice.InvoiceAmount);
-        Console.WriteLine("Customer name {0}", invoice.CustomerName);
-    }
-    public void SendEmail(string emailAddress, string message){
-        _sendEmail.Send(emailAddress, message);
-    }
-    public void GenerateFDF(string invoiceNumber){
-        _generateFDF.Generate(invoiceNumber);
-    }
-   
-}
+
+
 
 class Program
 {
