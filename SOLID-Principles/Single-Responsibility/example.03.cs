ea89c9b87cc9f8f7bfbcadc86d1f40620b7430c0
mamun2519 <juborajislam46@gmail.com>
2025-07-09 23:17:16 +0600

    added to single responsibility pattern

--- SOLID-Principles/Single-Responsibility/example.03.cs
+++ SOLID-Principles/Single-Responsibility/example.03.cs
@@ -20,15 +20,23 @@ class SendPushNotification : INotificationService{
                 Console.WriteLine("Push Notification: " + message);
         }
 }
+
+class NotificationManager {
+    private INotificationService _notificationService;
+    public NotificationManager(INotificationService notificationService){
+        _notificationService = notificationService;
+    }
+    public void Send (string message){
+        _notificationService.Send(message);
+    }
+}
 class Program
 {
     public static void Main(string[] args)
     {
 
-            INotificationService notificationService = new SendEmail();
-            notificationService.Send("Hello World");
-            notificationService = new SendSMS();
-            notificationService.Send("Hello World");
+               NotificationManager notificationManager = new NotificationManager(new SendEmail());
+               notificationManager.Send("Hello, World!");
       
 
      
