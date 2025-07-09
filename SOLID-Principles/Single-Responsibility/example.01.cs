92f9aa718a29a6f8846b9b70f4b41a1fc7012f72
mamun2519 <juborajislam46@gmail.com>
2025-07-09 22:21:17 +0600

    added to single responsibilty pattern

---
+++ SOLID-Principles/Single-Responsibility/example.01.cs
@@ -0,0 +1,54 @@
+using System;
+
+
+}
+class User
+{
+    public string name { get; set; }
+    public string email { get; set; }
+    public string password { get; set; }
+}
+
+class UserService
+{
+    public void register(User user)
+    {
+        // register user
+        Console.WriteLine("User registered", user.name);
+    }
+}
+
+class SendEmail
+{
+    public void sendEmail(User user)
+    {
+        // send email
+        Console.WriteLine("Email sent to", user.email);
+    }
+}
+
+
+class ReportFileSaver
+{
+    public void saveToFile(GoodReport report, string fileName)
+    {
+        System.IO.File.WriteAllText(fileName, report.content);
+    }
+}
+class Program
+{
+    public static void Main(string[] args)
+    {
+
+
+
+        User user = new User();
+        user.name = "John";
+        user.email = "john@doe.com";
+        user.password = "123456";
+        UserService userService = new UserService();
+        userService.register(user);
+        SendEmail sendEmail = new SendEmail();
+        sendEmail.sendEmail(user);
+    }
+}
