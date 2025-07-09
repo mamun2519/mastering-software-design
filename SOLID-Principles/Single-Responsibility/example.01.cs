0df4cdc08c3491ded02d8c09be30be04fe64acb0
mamun2519 <juborajislam46@gmail.com>
2025-07-09 22:22:07 +0600

    added to single responsibilty pattern

--- SOLID-Principles/Single-Responsibility/example.01.cs
+++ SOLID-Principles/Single-Responsibility/example.01.cs
@@ -2,20 +2,3 @@ using System;
 
 
 }
-class User
-{
-    public string name { get; set; }
-    public string email { get; set; }
-    public string password { get; set; }
-}
-
-class UserService
-{
-    public void register(User user)
-    {
-        // register user
-        Console.WriteLine("User registered", user.name);
-    }
-}
-
-
