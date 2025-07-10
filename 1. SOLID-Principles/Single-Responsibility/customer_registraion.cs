2e9776f76647e4a1bbb605d4522abd34d75aa4c0
mamun2519 <juborajislam46@gmail.com>
2025-07-10 12:12:50 +0600

    feat: add to customer registration follow srp principle

--- 1. SOLID-Principles/Single-Responsibility/customer_registraion.cs
+++ 1. SOLID-Principles/Single-Responsibility/customer_registraion.cs
@@ -7,11 +7,7 @@ class Program
 {
     public static void Main(string[] args)
     {
-        Customer customer = new Customer{ name = "John", email = "john@doe.com", phone ="029484954"};
-         CustomerManagement cm = new CustomerManagement();
-         cm.Save(customer);
-         EmailManagement em = new EmailManagement();
-         em.SendEmail(customer);
+        
         
         }
       
