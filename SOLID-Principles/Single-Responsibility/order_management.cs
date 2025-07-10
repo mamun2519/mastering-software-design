22af09fc157fabf35cf05b41ab0208e3713e6a61
mamun2519 <juborajislam46@gmail.com>
2025-07-10 11:49:48 +0600

    feat: add to order management follow SRS

---
+++ SOLID-Principles/Single-Responsibility/order_management.cs
@@ -0,0 +1,40 @@
+using System;
+
+class Order {
+    public string orderNumber { get; set; }
+    public string orderEmail { get; set; }
+}
+
+class OrderServices {
+    public void PlaceOrder(Order order){
+        Console.WriteLine("Order placed for " + order.orderEmail);
+    }
+}
+
+class OrderRepository {
+    public void Save(Order order){
+        Console.WriteLine("Order saved for " + order.orderEmail);
+    }
+}
+
+class EmailServices {
+    public void SendEmail(Order order){
+        Console.WriteLine("Email sent for " + order.orderEmail);
+    }
+}
+
+class Program
+{
+    public static void Main(string[] args)
+    {
+
+        Order order = new Order{ orderNumber = "123", orderEmail = "test@test.com"};
+        OrderServices orderServices = new OrderServices();
+        OrderRepository orderRepository = new OrderRepository();
+        EmailServices emailServices = new EmailServices();
+        orderServices.PlaceOrder(order);
+        orderRepository.Save(order);
+        emailServices.SendEmail(order);
+     
+    }
+}
