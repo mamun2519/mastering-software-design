ece08cf585d5944856cbef327ef0aedcdfa4801d
mamun2519 <juborajislam46@gmail.com>
2025-07-10 11:59:21 +0600

    added to blog manager and follow SRP Principle

--- SOLID-Principles/Single-Responsibility/bloog_manager.cs
+++ SOLID-Principles/Single-Responsibility/bloog_manager.cs
@@ -10,8 +10,6 @@ class Program
     {
 
      
-        Blog blog = new Blog{ Title = "My Blog", Author = "Juboraj Islam Mamun", Content = "This is my blog" };
-        PostServices postServices = new PostServices();
         postServices.createPost(blog);
         PostRepository postRepository = new PostRepository();
         postRepository.savePost(blog);
