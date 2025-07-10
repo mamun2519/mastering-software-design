48a3b1fb89f185270c056fbc15cc943cea9da8cd
mamun2519 <juborajislam46@gmail.com>
2025-07-10 11:50:40 +0600

    feat: add to order management follow SRS

--- SOLID-Principles/Single-Responsibility/order_management.cs
+++ SOLID-Principles/Single-Responsibility/order_management.cs
@@ -8,9 +8,7 @@ class Program
     public static void Main(string[] args)
     {
 
-       
-        OrderRepository orderRepository = new OrderRepository();
-        EmailServices emailServices = new EmailServices();
+ 
         orderServices.PlaceOrder(order);
         orderRepository.Save(order);
         emailServices.SendEmail(order);
