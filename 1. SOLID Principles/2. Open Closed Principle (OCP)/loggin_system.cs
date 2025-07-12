81a7b383a05408639b4d06f39fecea5d19fee37d
mamun2519 <juborajislam46@gmail.com>
2025-07-12 14:49:22 +0600

    added to open close principle

--- 1. SOLID Principles/2. Open Closed Principle (OCP)/loggin_system.cs
+++ 1. SOLID Principles/2. Open Closed Principle (OCP)/loggin_system.cs
@@ -10,10 +10,6 @@ class Program
     public static void Main(string[] args)
     {
 
-        LoggerService loggerService = new LoggerService(new ConsoleLogger());
-        loggerService.Log("Hello World");
-        loggerService = new LoggerService(new FileLogger());
-        loggerService.Log("Hello World");
     
     }
 
