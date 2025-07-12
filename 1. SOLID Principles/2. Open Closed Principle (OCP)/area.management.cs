4fbbce95b7846ce4187330ae76461d56e8e33caf
mamun2519 <juborajislam46@gmail.com>
2025-07-12 14:40:01 +0600

    added to open close principle

--- 1. SOLID Principles/2. Open Closed Principle (OCP)/area.management.cs
+++ 1. SOLID Principles/2. Open Closed Principle (OCP)/area.management.cs
@@ -1,6 +1,53 @@
 using System;
 
 
+// bed code
+class Shape {
+    public string Type;
+}
 
+class AreaCalculator {
+    public void CalculateArea(Shape shape) {
+        if (shape.Type == "Circle") {
+            // calculate for circle
+        } else if (shape.Type == "Rectangle") {
+            // calculate for rectangle
+        }
+        // etc...
+    }
+}
+// good code
+interface IShape {
+    void CalculateArea();
+}
 
+class Circle : IShape {
+        public void CalculateArea(){
+            // calculate for circle
+            Console.WriteLine("Circle");
+        }
+}
 
+class Rectangle : IShape{
+        public void CalculateArea(){
+            // calculate for rectangle
+            Console.WriteLine("Rectangle");
+        }
+}
+class Triangle : IShape{
+        public void CalculateArea(){
+            // calculate for triangle
+            Console.WriteLine("Triangle");
+        }
+}
+class Program
+{
+    public static void Main(string[] args)
+    {
+
+        IShape shape = new Circle();
+        shape.CalculateArea();
+    
+    }
+
+}
