7820647c92663bd8d44ab4049a277a786b5c8690
mamun2519 <juborajislam46@gmail.com>
2025-07-12 15:19:33 +0600

    added to open close principle

--- 1. SOLID Principles/2. Open Closed Principle (OCP)/discount_calculator.cs
+++ 1. SOLID Principles/2. Open Closed Principle (OCP)/discount_calculator.cs
@@ -2,8 +2,26 @@ using System;
 
 // bed code
 
+class PremiumCustomer : ICustomer{
+    public void CalculateDiscount(double price, int quantity, string method){
+        Console.WriteLine(price * quantity * 0.8);
+    }
+}
+class VIPCustomer : ICustomer{
+    public void CalculateDiscount(double price, int quantity, string method){
+        Console.WriteLine(price * quantity * 0.7);
+    }
+}
 
-
+class DiscountServices{
+    private ICustomer _customer;
+    public DiscountServices(ICustomer customer){
+        _customer = customer;
+    }
+    public void CalculateDiscount(double price, int quantity, string method){
+        _customer.CalculateDiscount(price, quantity, method);
+    }
+}
 class Program
 {
     public static void Main(string[] args)
