e148887443b72c8a3bc4be9b0321d4c9acbd774a
mamun2519 <juborajislam46@gmail.com>
2025-07-16 15:40:05 +0600

    added to Dependency Inversion Principle (DIP)

---
+++ 1. SOLID Principles/5. Dependency Inversion Principle (DIP)/Report_Generator.cs
@@ -0,0 +1,42 @@
+using System;
+
+interface IReportGenerator {
+    void generateReport();
+}
+
+class pdfReportGenerator : IReportGenerator{
+    public void generateReport(){
+        Console.WriteLine("Generating PDF Report");
+    }
+}
+class ExcelReportGenerator : IReportGenerator{
+    public void generateReport(){
+        Console.WriteLine("Generating Excel Report");
+    }
+}
+
+class ReportService {
+    private IReportGenerator reportGenerator;
+    public ReportService (IReportGenerator reportGenerator){
+        this.reportGenerator = reportGenerator;
+    }
+    public void generateReport(){
+        reportGenerator.generateReport();
+    }
+}
+
+class Program
+{
+    public static void Main(string[] args)
+    {
+       
+      
+        IReportGenerator pdfReportGenerator = new pdfReportGenerator();
+        IReportGenerator excelReportGenerator = new ExcelReportGenerator();
+
+        ReportService reportService = new ReportService(pdfReportGenerator);
+        reportService.generateReport();
+        reportService = new ReportService(excelReportGenerator);
+    }
+
+}
