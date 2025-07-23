2ef1abaeb64a913082974ecaa1aedc55cca5ec0a
mamun2519 <juborajislam46@gmail.com>
2025-07-23 21:59:07 +0600

    Concept: added to Singleton Design Pattern

--- 6. Singleton Design Pattern/database_management.cs
+++ 6. Singleton Design Pattern/database_management.cs
@@ -4,14 +4,7 @@ using System.Collections.Generic;
 public class Database {
         private static Database instance  = new Database();
 
-        private Database (){
-                Console.WriteLine("Database created");
-        }
-
-        public static Database getInstance(){
-                return instance;
-        }
-
+       
         
 
 class Program
