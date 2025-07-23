1e57419a2daa6238a7bba8a176371a5f0b343cca
mamun2519 <juborajislam46@gmail.com>
2025-07-23 21:53:56 +0600

    Concept: added to Singleton Design Pattern

--- 6. Singleton Design Pattern/Config_Manager.cs
+++ 6. Singleton Design Pattern/Config_Manager.cs
@@ -1,18 +1,25 @@
 using System;
+using System.Collections.Generic;
 
-class ConfigManager {
-        private static ConfigManager _instance;
+public  class ConfigManager
+{
+    private static readonly ConfigManager instance = new ConfigManager();
+    private Dictionary<string, string> settings = new Dictionary<string, string>();
 
-        private ConfigManager () { }
+    private ConfigManager() { }
 
-        public ConfigManager Instance (){
-                if (_instance == null)
-                {
-                    _instance = new ConfigManager();
-                }
-                return _instance;
-        }
-} 
+    public static ConfigManager Instance => instance;
+
+    public void Set(string key, string value)
+    {
+        settings[key] = value;
+    }
+
+    public string Get(string key)
+    {
+        return settings.ContainsKey(key) ? settings[key] : null;
+    }
+}
 
 class Program
 {
@@ -21,9 +28,13 @@ class Program
     public static void Main(string[] args)
     {
 
-           
-           
-                
+        ConfigManager config = ConfigManager.Instance;
+        config.Set("API_KEY", "837438743");
+        config.Set("API_URL", "https://api.example.com");
+        Console.WriteLine(config.Get("API_KEY")); // Output: 837438743
+        Console.WriteLine(config.Get("API_URL")); // Output: https://api.example.com
+
+
 
     }
 }
