1268421d2fff0d5ae06c62e7ad2877a4f999fc05
mamun2519 <juborajislam46@gmail.com>
2025-07-26 12:04:52 +0600

    Concept: added to Abstract Factory Design Pattern

--- 8. Abstract Factory Design Pattern/example.cs
+++ 8. Abstract Factory Design Pattern/example.cs
@@ -5,18 +5,7 @@ using System;
 
 
 
-class Application {
-        private IButton button;
-        private ICheckbox checkbox;
-        public Application (IGUIFactory factory){
-                        button = factory.CreateButton();
-                        checkbox = factory.CreateCheckbox();
-        }
-        public void Paint(){
-                button.Print();
-                checkbox.Print();
-        }
-}
+
 class Program
 {
 
