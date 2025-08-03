f34932462adb2951d02b8c456077e8b8a6c75131
mamun2519 <juborajislam46@gmail.com>
2025-08-03 16:01:46 +0600

    concept: added to onion achitecure

--- 11. Onion Architecture with SOLID/services.cs
+++ 11. Onion Architecture with SOLID/services.cs
@@ -16,25 +16,6 @@ using System.Collections.Generic;
 
 
 
-public class CourseRepository : ICourseRepository {
-        Database db;
-        public CourseRepository(Database db){
-                this.db = db;
-        }
-        public void AddCourse(Course course){
-                db.Courses.Add(course);
-        }
-        public void RemoveCourse(Course course){
-                db.Courses.Remove(course);
-        }
-        public void UpdateCourse(Course course){
-                db.Courses[db.Courses.IndexOf(course)] = course;
-        }
-        public IList< Course> GetAllCourses(){
-                return db.Courses;
-        }
-}
-
 
 // services interfaces
 public interface IStudentService {
