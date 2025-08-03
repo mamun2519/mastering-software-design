816a75c8ca1aaf5f1675f6cc685e139d06f861bf
mamun2519 <juborajislam46@gmail.com>
2025-08-03 15:59:17 +0600

    concept: added to onion achitecure

--- 11. Onion Architecture with SOLID/services.cs
+++ 11. Onion Architecture with SOLID/services.cs
@@ -1,12 +1,7 @@
 using System;
 using System.Collections.Generic;
 
-// modules
-public class Student {
-        public int StudentId { get; set; }
-        public string Name { get; set; }
-        public string Email { get; set; }
-}
+
 
 public class Trainer {
         public int TrainerId { get; set; }
