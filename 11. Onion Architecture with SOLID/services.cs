0f54f522cbfb899d3cfe0c9855cced816c606983
mamun2519 <juborajislam46@gmail.com>
2025-08-03 16:03:09 +0600

    concept: added to onion achitecure

--- 11. Onion Architecture with SOLID/services.cs
+++ 11. Onion Architecture with SOLID/services.cs
@@ -26,7 +26,6 @@ class Program
     {
 
        // Create instances of repositories and services
-        Database db = new Database();
             IStudentRepository studentRepository = new StudentRepository(db);
             ITrainerRepository trainerRepository = new TrainerRepository(db);
             ICourseRepository courseRepository = new CourseRepository(db);
